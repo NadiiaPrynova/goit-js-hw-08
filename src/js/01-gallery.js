8174db4e732788f66f61460384b6199caa4ca543
Nadiia PRYNOVA <112952706+NadiiaPrynova@users.noreply.github.com>
2023-08-10 19:37:54 +0300

    first taskcommit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,18 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
+const container = document.querySelector('.gallery');
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`).join('')
+}
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+// Change code below this line
+console.log(galleryItems);
