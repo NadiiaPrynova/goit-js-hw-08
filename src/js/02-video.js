f49de2bfc0911dc44949a899302324e7bb1bc3a4
Nadiia PRYNOVA <112952706+NadiiaPrynova@users.noreply.github.com>
2023-08-13 09:30:53 +0300

    second commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
+
+  const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+function saveTime(obj) {
+    const seconds = obj.seconds;
+    console.log(seconds);
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds))
+    }
+const timeVideo = localStorage.getItem('videoplayer-current-time');
+console.log(timeVideo);
+player.on('timeupdate', throttle(saveTime, 1000)); 
+player.setCurrentTime(timeVideo);
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    input: document.querySelector('.feedback-form input')
+};
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+refs.input.addEventListener('input', throttle(onEmailInput, 500));
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+    localStorage.removeItem('feedback-email-state');
+}
+
+function onTextareaInput(evt) {
+    const message = evt.target.value;
+    localStorage.setItem('feedback-form-state', message);
+}
+
+function onEmailInput(evt) {
+    const emailName = evt.target.value;
+    localStorage.setItem('feedback-email-state', JSON.stringify(emailName));
+}
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem('feedback-form-state');
+    if (savedMessage) {
+        console.log(savedMessage);
+        refs.textarea.value = savedMessage;
+    }
+}
+
+function populateEmailInput() {
+    const savedEmailName = localStorage.getItem('feedback-email-state');
+    if (savedEmailName) {
+        console.log(savedEmailName);
+        refs.input.value = savedEmailName;
+        
+    }
+}
 
